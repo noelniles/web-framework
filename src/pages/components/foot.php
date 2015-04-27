6beb11874f0a186d1e1559d487c1678e02b5939d
Noel Niles <shakabra321@gmail.com>
2015-04-27 01:00:34 -0400

    Add GPG public key fingerprint to footer.

--- src/pages/components/foot.php
+++ src/pages/components/foot.php
@@ -1,16 +1,16 @@
 <footer class="page-footer grey lighten-5">
   <div class="container">
     <div class="row">
-      <div class="col l6 s12">
+      <div class="col s12">
         <h5 class="grey-text text-lighten-1">noelniles.com</h5>
         <a href="mailto:noelniles@gmail.com" class="grey-text text-lighten-1">noelniles@gmail.com</a>
+        <p class="grey-text text-lighten-1">GPG Fingerprint: 5555 7B13 0041 2CCE 8C8E  D576 C569 AC62 7206 B04A</p> 
       </div>
     </div>
   </div>
   <div class="footer-copyright">
     <div class="container">
     <span class="grey-text">© 2015 Copyright Noel Niles</span>
-    <a class="grey-text text-darken-1 right" href="#!">More Links</a>
     </div>
   </div>
 </footer>
